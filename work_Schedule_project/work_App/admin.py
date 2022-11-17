590c120870acc6220c66e436d5f9bf144efcd79c
unknown <okaforchikelue93@gmail.com>
2022-11-17 16:17:23 +0100

    register the models in the admin file

--- work_Schedule_project/work_App/admin.py
+++ work_Schedule_project/work_App/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Staff, Staff_Grade
 
 # Register your models here.
+class StaffAdmin(admin.ModelAdmin):
+    Listdisplay=('FirstName', 'LastName', 'Gross_Salary')
+
+class StaffGradeAdmin(admin.ModelAdmin):
+    Listdisplay=('Position', 'Basic_Salary')
+
+admin.site.register(Staff_Grade, StaffGradeAdmin)
+admin.site.register(Staff, StaffAdmin)
